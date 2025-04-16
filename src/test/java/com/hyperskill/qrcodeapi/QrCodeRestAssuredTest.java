acd0003fac74e71e3791779fcaf9e13b0db75c93
Jovan Stepanovic <jsjovan94@gmail.com>
2025-04-16 22:50:58 +0200

    add new test cases

--- src/test/java/com/hyperskill/qrcodeapi/QrCodeRestAssuredTest.java
+++ src/test/java/com/hyperskill/qrcodeapi/QrCodeRestAssuredTest.java
@@ -1,6 +1,7 @@
 package com.hyperskill.qrcodeapi;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -54,6 +56,98 @@ class QrCodeRestAssuredTest {
                 .get("/api/qrcode")
                 .then()
                 .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    public void testValidQrCodeGeneration() {
+        given()
+                .queryParam("contents", "abcdef")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType("image/png");
+    }
+
+    @Test
+    public void testInvalidCorrectionParam() {
+        given()
+                .queryParam("contents", "abcde")
+                .queryParam("correction", "S")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("error", equalTo("Permitted error correction levels are L, M, Q, H"));
+    }
+
+    @Test
+    public void testBlankContents() {
+        given()
+                .queryParam("contents", "")
+                .queryParam("correction", "S")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("error", equalTo("Contents cannot be null or blank"));
+    }
+
+    @Test
+    public void testInvalidCorrectionAndType() {
+        given()
+                .queryParam("contents", "abcde")
+                .queryParam("correction", "S")
+                .queryParam("type", "tiff")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("error", equalTo("Permitted error correction levels are L, M, Q, H"));
+    }
+
+    @Test
+    public void testAllParamsValid() {
+        given()
+                .queryParam("contents", "hello")
+                .queryParam("size", 300)
+                .queryParam("correction", "H")
+                .queryParam("type", "png")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.OK.value())
+                .contentType("image/png");
+    }
+
+    @Test
+    public void testInvalidSizeWithValidCorrection() {
+        given()
+                .queryParam("contents", "abc")
+                .queryParam("size", -100)
+                .queryParam("correction", "H")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("error", equalTo("Image size must be between 150 and 350 pixels"));
+    }
+
+    @Test
+    public void testInvalidTypeOnly() {
+        given()
+                .queryParam("contents", "abcde")
+                .queryParam("type", "tiff")
+                .when()
+                .get("/api/qrcode")
+                .then()
+                .statusCode(HttpStatus.BAD_REQUEST.value())
+                .contentType(ContentType.JSON)
+                .body("error", equalTo("Only png, jpeg and gif image types are supported"));
     }
 
 }
