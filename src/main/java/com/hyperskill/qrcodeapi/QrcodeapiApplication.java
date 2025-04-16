60c16bbf8faa99c715c26e885f5dd24561d789ae
Jovan Stepanovic <jsjovan94@gmail.com>
2025-04-16 15:10:15 +0200

    add basic project structure

---
+++ src/main/java/com/hyperskill/qrcodeapi/QrcodeapiApplication.java
@@ -0,0 +1,13 @@
+package com.hyperskill.qrcodeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyperskill/qrcodeapi/QrcodeapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyperskill.qrcodeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
